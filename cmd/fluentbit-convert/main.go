189dfe841b0c4ecd26e4c4f830640ebfdf6c215f
Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>
2025-05-15 10:43:01 -0400

    fix: close output file with defer.
    Signed-off-by: Phillip Adair Stewart Whelan <phillip.whelan@chronosphere.io>

--- cmd/fluentbit-convert/main.go
+++ cmd/fluentbit-convert/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
@@ -14,7 +13,7 @@ import (
 func main() {
 	var outputFormat string
 	var outputFilename string
-	var outfile io.Writer
+	var outfile *os.File
 
 	pflag.StringVarP(&outputFormat, "format", "f", "yaml",
 		"output format, one of: json, yaml, ini")
@@ -55,6 +54,7 @@ func main() {
 			fmt.Printf("ERROR: %s\n", err)
 			os.Exit(1)
 		}
+		defer outfile.Close()
 	} else {
 		outfile = os.Stdout
 	}
