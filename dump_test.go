1e98576fb452470a77895f1ffc8e80370253f0ab
Alec Holmes <alec@chronosphere.io>
2025-05-23 13:12:16 -0700

    Fix YAML and JSON plugin marshalling

--- dump_test.go
+++ dump_test.go
@@ -1,12 +1,15 @@
 package fluentbitconfig
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"testing"
 
-	"github.com/alecthomas/assert/v2"
 	"github.com/muesli/reflow/dedent"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/calyptia/go-fluentbit-config/v2/property"
 )
@@ -18,7 +21,7 @@ func TestParseAsYAML(t *testing.T) {
 				inputs:
 					- name: dummy
 		`))
-		assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: field pipelines not found in type fluentbitconfig.Config")
+		require.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: field pipelines not found in type fluentbitconfig.Config")
 	})
 
 	t.Run("unknown_section", func(t *testing.T) {
@@ -26,7 +29,7 @@ func TestParseAsYAML(t *testing.T) {
 			unknown:
 				- name: dummy
 		`))
-		assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: field unknown not found in type fluentbitconfig.Config")
+		require.EqualError(t, err, "yaml: unmarshal errors:\n  line 1: field unknown not found in type fluentbitconfig.Config")
 	})
 
 	t.Run("unknown_pipeline_plugins_kind", func(t *testing.T) {
@@ -35,13 +38,13 @@ func TestParseAsYAML(t *testing.T) {
 				foos:
 					- name: bar
 		`))
-		assert.EqualError(t, err, "yaml: unmarshal errors:\n  line 2: field foos not found in type fluentbitconfig.Pipeline")
+		require.EqualError(t, err, "yaml: unmarshal errors:\n  line 2: field foos not found in type fluentbitconfig.Pipeline")
 	})
 
 	t.Run("empty", func(t *testing.T) {
 		got, err := ParseAsYAML("")
-		assert.NoError(t, err)
-		assert.Equal(t, Config{}, got)
+		require.NoError(t, err)
+		require.Equal(t, Config{}, got)
 	})
 
 	t.Run("ok", func(t *testing.T) {
@@ -52,8 +55,8 @@ func TestParseAsYAML(t *testing.T) {
 		`)
 		fmt.Printf("text: %q\n", text)
 		cfg, err := ParseAsYAML(text)
-		assert.NoError(t, err)
-		assert.Equal(t, Config{
+		require.NoError(t, err)
+		require.Equal(t, Config{
 			Pipeline: Pipeline{
 				Inputs: []Plugin{
 					{
@@ -70,6 +73,280 @@ func TestParseAsYAML(t *testing.T) {
 			},
 		}, cfg)
 	})
+}
+
+func TestMarshaling(t *testing.T) {
+	testCases := []struct {
+		name          string
+		config        Config
+		expectClassic string
+		expectYAML    string
+		expectJSON    string
+	}{
+		{
+			name:          "empty",
+			config:        Config{},
+			expectClassic: "",
+			expectYAML:    `{}`,
+			expectJSON: `
+{
+  "pipeline": {}
+}
+`,
+		},
+		{
+			name: "partial",
+			config: Config{
+				Customs: []Plugin{
+					{
+						ID:   "test-id",
+						Name: "test-name",
+						Properties: []property.Property{
+							{Key: "test-key", Value: "test-value"},
+						},
+					},
+				},
+				Pipeline: Pipeline{
+					Inputs: []Plugin{
+						{Name: "dummy"},
+						{},
+					},
+					Outputs: []Plugin{
+						{},
+					},
+				},
+			},
+			expectClassic: `
+[CUSTOM]
+    Name     test-name
+    test-key test-value
+[INPUT]
+    Name dummy
+`,
+			expectYAML: `
+customs:
+    - name: test-name
+      test-key: test-value
+pipeline:
+    inputs:
+        - name: dummy
+        - null
+    outputs:
+        - null
+`,
+			expectJSON: `
+{
+  "customs": [
+    {
+      "name": "test-name",
+      "test-key": "test-value"
+    }
+  ],
+  "pipeline": {
+    "inputs": [
+      {
+        "name": "dummy"
+      },
+      null
+    ],
+    "outputs": [
+      null
+    ]
+  }
+}
+`,
+		},
+		{
+			name: "full",
+			config: Config{
+				Env: []property.Property{
+					{Key: "Apple", Value: "aa"},
+					{Key: "banana", Value: "BB"},
+				},
+				Includes: []string{"i1", "i2"},
+				Service: []property.Property{
+					{Key: "s1", Value: "hello world"},
+				},
+				Customs: []Plugin{
+					{
+						Name: "my custom plugin",
+						Properties: []property.Property{
+							{Key: "this is a key", Value: "this is a value"},
+						},
+					},
+				},
+				Pipeline: Pipeline{
+					Inputs: []Plugin{
+						{
+							Name: "input 1",
+							Properties: []property.Property{
+								{Key: "a", Value: "a a"},
+								{Key: "b", Value: "b b"},
+							},
+						},
+						{
+							Name: "input 2",
+							Properties: []property.Property{
+								{Key: "c", Value: "CC"},
+							},
+						},
+					},
+					Filters: []Plugin{
+						{
+							Name: "filter-1",
+							Properties: []property.Property{
+								{Key: "e", Value: "EE"},
+							},
+						},
+					},
+					Outputs: []Plugin{
+						{
+							Name: "output-1",
+							Properties: []property.Property{
+								{Key: "f", Value: "ff \"with a quote\""},
+							},
+						},
+					},
+				},
+				Parsers: []Plugin{
+					{
+						Name: "my-parser",
+						Properties: []property.Property{
+							{Key: "g", Value: "GG"},
+						},
+					},
+				},
+			},
+			expectClassic: `
+@SET Apple=aa
+@SET banana=BB
+@INCLUDE i1
+@INCLUDE i2
+[SERVICE]
+    s1 hello world
+[CUSTOM]
+    Name          my custom plugin
+    this is a key this is a value
+[INPUT]
+    Name input 1
+    a    a a
+    b    b b
+[INPUT]
+    Name input 2
+    c    CC
+[FILTER]
+    Name filter-1
+    e    EE
+[OUTPUT]
+    Name output-1
+    f    ff "with a quote"
+[PARSER]
+    Name my-parser
+    g    GG
+`,
+			expectYAML: `
+env:
+    Apple: aa
+    banana: BB
+includes:
+    - i1
+    - i2
+service:
+    s1: hello world
+customs:
+    - name: my custom plugin
+      this is a key: this is a value
+pipeline:
+    inputs:
+        - name: input 1
+          a: a a
+          b: b b
+        - name: input 2
+          c: CC
+    filters:
+        - name: filter-1
+          e: EE
+    outputs:
+        - name: output-1
+          f: ff "with a quote"
+parsers:
+    - name: my-parser
+      g: GG
+`,
+			expectJSON: `
+{
+  "env": {
+    "Apple": "aa",
+    "banana": "BB"
+  },
+  "includes": [
+    "i1",
+    "i2"
+  ],
+  "service": {
+    "s1": "hello world"
+  },
+  "customs": [
+    {
+      "name": "my custom plugin",
+      "this is a key": "this is a value"
+    }
+  ],
+  "pipeline": {
+    "inputs": [
+      {
+        "name": "input 1",
+        "a": "a a",
+        "b": "b b"
+      },
+      {
+        "name": "input 2",
+        "c": "CC"
+      }
+    ],
+    "filters": [
+      {
+        "name": "filter-1",
+        "e": "EE"
+      }
+    ],
+    "outputs": [
+      {
+        "name": "output-1",
+        "f": "ff \"with a quote\""
+      }
+    ]
+  },
+  "parsers": [
+    {
+      "name": "my-parser",
+      "g": "GG"
+    }
+  ]
+}
+`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualClassic, err := tc.config.DumpAsClassic()
+			require.NoError(t, err)
+			assert.Equal(t, strings.TrimSpace(tc.expectClassic), strings.TrimSpace(actualClassic))
+
+			actualYAML, err := tc.config.DumpAsYAML()
+			require.NoError(t, err)
+			assert.Equal(t, strings.TrimSpace(tc.expectYAML), strings.TrimSpace(actualYAML))
+
+			// Using a JSON encoder instead of DumpAsJSON for better readability with indented multiline JSON
+			var buf bytes.Buffer
+			jsonEnc := json.NewEncoder(&buf)
+			jsonEnc.SetIndent("", "  ")
+			jsonEnc.SetEscapeHTML(false)
+			require.NoError(t, jsonEnc.Encode(tc.config))
+			assert.Equal(t, strings.TrimSpace(tc.expectJSON), strings.TrimSpace(buf.String()))
+		})
+	}
 }
 
 func configLiteral(s string) string {
--- go.mod
+++ go.mod
@@ -1,17 +1,22 @@
 module github.com/calyptia/go-fluentbit-config/v2
 
-go 1.22.4
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
-	github.com/alecthomas/assert/v2 v2.10.0
+	github.com/alecthomas/assert/v2 v2.11.0
 	github.com/muesli/reflow v0.3.0
 	github.com/spf13/pflag v1.0.6
-	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
-	golang.org/x/mod v0.18.0
+	github.com/stretchr/testify v1.10.0
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6
+	golang.org/x/mod v0.24.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/alecthomas/repr v0.4.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hexops/gotextdiff v1.0.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
