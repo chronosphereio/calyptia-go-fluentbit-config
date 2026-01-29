77ca3fe89aa23741a0509337a88b3d7988493d82
calyptia-ci <98808777+calyptia-ci@users.noreply.github.com>
2026-01-29 11:30:08 -0800

    Update schema to latest (#180)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: alecholmes <179268+alecholmes@users.noreply.github.com>

--- schema.go
+++ schema.go
@@ -13,7 +13,7 @@ import (
 //go:embed schemas/*.json
 var rawSchemas embed.FS
 
-//go:embed schemas/26.1.5.json
+//go:embed schemas/26.1.6.json
 var rawSchema []byte
 
 var DefaultSchema = func() Schema {
